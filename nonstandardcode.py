dc65737ea7f61c9fcb8b12df7ba50ce73903c07e
Mohit-TA <mohit.pareek@tigeranalytics.com>
2023-07-25 16:13:43 +0530

    Few more changes according to flake8

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -62,8 +62,10 @@ compare_props = pd.DataFrame({
     "Stratified": income_cat_proportions(strat_test_set),
     "Random": income_cat_proportions(test_set),
 }).sort_index()
-compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
-compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
+compare_props["Rand. %error"] = 100 * compare_props["Random"] / \
+    compare_props["Overall"] - 100
+compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / \
+    compare_props["Overall"] - 100
 
 for set_ in (strat_train_set, strat_test_set):
     set_.drop("income_cat", axis=1, inplace=True)
@@ -74,11 +76,15 @@ housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
 
 corr_matrix = housing.corr()
 corr_matrix["median_house_value"].sort_values(ascending=False)
-housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
-housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
-housing["population_per_household"] = housing["population"]/housing["households"]
+housing["rooms_per_household"] = housing["total_rooms"] / \
+                                        housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"] / \
+                                        housing["total_rooms"]
+housing["population_per_household"] = housing["population"] / \
+                                            housing["households"]
 
-housing = strat_train_set.drop("median_house_value", axis=1)  # drop labels for training set
+# drop labels for training set
+housing = strat_train_set.drop("median_house_value", axis=1)
 housing_labels = strat_train_set["median_house_value"].copy()
 
 imputer = SimpleImputer(strategy="median")
@@ -90,12 +96,16 @@ X = imputer.transform(housing_num)
 
 housing_tr = pd.DataFrame(X, columns=housing_num.columns,
                           index=housing.index)
-housing_tr["rooms_per_household"] = housing_tr["total_rooms"]/housing_tr["households"]
-housing_tr["bedrooms_per_room"] = housing_tr["total_bedrooms"]/housing_tr["total_rooms"]
-housing_tr["population_per_household"] = housing_tr["population"]/housing_tr["households"]
+housing_tr["rooms_per_household"] = housing_tr["total_rooms"] / \
+                                        housing_tr["households"]
+housing_tr["bedrooms_per_room"] = housing_tr["total_bedrooms"] / \
+                                        housing_tr["total_rooms"]
+housing_tr["population_per_household"] = housing_tr["population"] / \
+                                        housing_tr["households"]
 
 housing_cat = housing[['ocean_proximity']]
-housing_prepared = housing_tr.join(pd.get_dummies(housing_cat, drop_first=True))
+housing_prepared = housing_tr.join(pd.get_dummies(housing_cat,
+                                                  drop_first=True))
 
 
 lin_reg = LinearRegression()
@@ -127,8 +137,11 @@ param_distribs = {
     }
 
 forest_reg = RandomForestRegressor(random_state=42)
-rnd_search = RandomizedSearchCV(forest_reg, param_distributions=param_distribs,
-                                n_iter=10, cv=5, scoring='neg_mean_squared_error', random_state=42)
+rnd_search = RandomizedSearchCV(forest_reg,
+                                param_distributions=param_distribs,
+                                n_iter=10, cv=5,
+                                scoring='neg_mean_squared_error',
+                                random_state=42)
 rnd_search.fit(housing_prepared, housing_labels)
 cvres = rnd_search.cv_results_
 for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
@@ -144,8 +157,10 @@ param_grid = [
 
 forest_reg = RandomForestRegressor(random_state=42)
 # train across 5 folds, that's a total of (12+6)*5=90 rounds of training
-grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
-                           scoring='neg_mean_squared_error', return_train_score=True)
+grid_search = GridSearchCV(forest_reg,
+                           param_grid, cv=5,
+                           scoring='neg_mean_squared_error',
+                           return_train_score=True)
 grid_search.fit(housing_prepared, housing_labels)
 
 grid_search.best_params_
@@ -164,14 +179,19 @@ y_test = strat_test_set["median_house_value"].copy()
 
 X_test_num = X_test.drop('ocean_proximity', axis=1)
 X_test_prepared = imputer.transform(X_test_num)
-X_test_prepared = pd.DataFrame(X_test_prepared, columns=X_test_num.columns,
+X_test_prepared = pd.DataFrame(X_test_prepared,
+                               columns=X_test_num.columns,
                                index=X_test.index)
-X_test_prepared["rooms_per_household"] = X_test_prepared["total_rooms"]/X_test_prepared["households"]
-X_test_prepared["bedrooms_per_room"] = X_test_prepared["total_bedrooms"]/X_test_prepared["total_rooms"]
-X_test_prepared["population_per_household"] = X_test_prepared["population"]/X_test_prepared["households"]
+X_test_prepared["rooms_per_household"] = X_test_prepared["total_rooms"] / \
+                                            X_test_prepared["households"]
+X_test_prepared["bedrooms_per_room"] = X_test_prepared["total_bedrooms"] / \
+                                            X_test_prepared["total_rooms"]
+X_test_prepared["population_per_household"] = X_test_prepared["population"] / \
+                                                X_test_prepared["households"]
 
 X_test_cat = X_test[['ocean_proximity']]
-X_test_prepared = X_test_prepared.join(pd.get_dummies(X_test_cat, drop_first=True))
+X_test_prepared = X_test_prepared.join(pd.get_dummies(X_test_cat,
+                                                      drop_first=True))
 
 
 final_predictions = final_model.predict(X_test_prepared)
