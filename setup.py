a1adb06278a82ee3e9e6df5ee11f77d1646e8733
Mohit-TA <101808800+Mohit-TA@users.noreply.github.com>
2023-09-07 23:05:49 +0530

    Final code for Assignment 2.1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mle_project',
+    version='2.1',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.11.4"',
+    ],
+)
